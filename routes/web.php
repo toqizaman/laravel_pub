d8e39c0ce64980ce8dace8a7bd70952468c2824e
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-08-24 09:24:17 -0700

    moved the code to controller

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Models\Task;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Http\Request;
+use App\Http\Controllers\TaskControler;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,49 +14,20 @@ use Illuminate\Http\Request;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('tasks');
-});
-
-/**
- * Display All Tasks
- */
-Route::get('/', function () {
-    $tasks = Task::orderBy('created_at', 'asc')->get();
- 
-    return view('tasks', [
-        'tasks' => $tasks
-    ]);
-});
+Route::get('/', [TaskControler::class, 'index']);
  
 /**
  * Add A New Task
  */
-Route::post('/task', function (Request $request) {
-    $validator = Validator::make($request->all(), [
-        'name' => 'required|max:255',
-    ]);
- 
-    if ($validator->fails()) {
-        return redirect('/')
-            ->withInput()
-            ->withErrors($validator);
-    }
-    //
-    $task = new Task;
-    $task->name = $request->name;
-    $task->save();
- 
-    return redirect('/');
-});
+Route::post('/task', [TaskControler::class, 'store']);
  
 /**
  * Delete An Existing Task
  */
-Route::delete('/task/{id}', function ($id) {
-    //
-    Task::findOrFail($id)->delete();
+Route::delete('/task/{id}', [TaskControler::class, 'destroy']);
+// Route::delete('/task/{id}', function ($id) {
+//     //
+//     Task::findOrFail($id)->delete();
  
-    return redirect('/');
-});
+//     return redirect('/');
+// });
