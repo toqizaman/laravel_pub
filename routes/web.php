7318a79a4f83bae43ac39445db8de6ee03051176
Hamzaqureshi401 <46222436+Hamzaqureshi401@users.noreply.github.com>
2023-08-24 08:15:43 -0700

    prepping db

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
-
+use App\Models\Task;
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +15,47 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('tasks');
+});
+
+/**
+ * Display All Tasks
+ */
+Route::get('/', function () {
+    $tasks = Task::orderBy('created_at', 'asc')->get();
+ 
+    return view('tasks', [
+        'tasks' => $tasks
+    ]);
+});
+ 
+/**
+ * Add A New Task
+ */
+Route::post('/task', function (Request $request) {
+    $validator = Validator::make($request->all(), [
+        'name' => 'required|max:255',
+    ]);
+ 
+    if ($validator->fails()) {
+        return redirect('/')
+            ->withInput()
+            ->withErrors($validator);
+    }
+    //
+    $task = new Task;
+    $task->name = $request->name;
+    $task->save();
+ 
+    return redirect('/');
+});
+ 
+/**
+ * Delete An Existing Task
+ */
+Route::delete('/task/{id}', function ($id) {
+    //
+    Task::findOrFail($id)->delete();
+ 
+    return redirect('/');
 });
